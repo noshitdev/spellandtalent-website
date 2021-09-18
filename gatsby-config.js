86b446f9860ce300ff2cf350ec3080fb6207c243
mieszkos <lukasz@blossomeducational.com>
2021-09-18 17:50:43 +0200

    workflow change, fix excludes gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -58,14 +58,13 @@ module.exports = {
           'G-M86QYKLNDF',
         ],
         gtagConfig: {
-          optimize_id: 'OPT_CONTAINER_ID',
           anonymize_ip: true,
           cookie_expires: 0,
         },
         pluginConfig: {
           head: false,
           respectDNT: true,
-          exclude: [''],
+          exclude: [],
         },
       },
     },
